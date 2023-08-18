3f782e00b90d7665f16082c91ad1dc3a31b6e022
DanyaIT <105100908+DanyaIT@users.noreply.github.com>
2023-08-18 12:48:08 +0300

    add addFormComment change userProfile

--- src/Page/NotFoundPage/ui/NotFoundPage.tsx
+++ src/Page/NotFoundPage/ui/NotFoundPage.tsx
@@ -13,7 +13,7 @@ export const NotFoundPage: FC<NotFoundPageProps> = ({ className }) => {
 
     return (
         <div className={classNames(cls.NotFoundPage, {}, [className || ''])}>
-             {t('Страница не найдена')}
+             {t('Страница не найденаа')}
         </div>
     )
 }
